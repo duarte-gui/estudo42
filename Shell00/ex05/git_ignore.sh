039a6462476e0c384b0fe81c38e6b0a8f22f469f
duarte-gui <td.guilherme@duito.com.br>
2021-06-06 03:15:33 -0300

    upando tudo

---
+++ Shell00/ex05/git_ignore.sh
@@ -0,0 +1,4 @@
+cat .gitignore #se voce nao tem um desses, tá commitando senha pra geral
+
+#eu sei que a norminette não passa, mas assim eu acho a melhor forma pra não enviar "coisinhas", 
+#porém funciona "pro enunciado"
