e4216ed23a3108a694ccb72ff68ed8988c25d401
duarte-gui <td.guilherme@duito.com.br>
2021-06-06 05:38:07 -0300

    Subindo aulas feitas

---
+++ estudo_c/aula11-20/aula11.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int contador = 1; //1 igual é pra variável
+//  enquanto contador for menor ou igual
+    while(contador == 5) //2 == é pra igual de função (matemático)
+    {
+        printf("%i\n", contador);
+        ++contador; //++ atrás, adiciona 1 ao contador
+
+    }
+
+
+
+    return 0;
+}
