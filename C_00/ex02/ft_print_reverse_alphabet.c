565628907f02f3c518b7584b0f5d31f5de6cbb68
gutavare <gutavare@student.42sp.org.br>
2021-06-15 06:18:52 +0000

    Subindo C00

---
+++ C_00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gutavare <gutavare@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/09 01:38:45 by gutavare          #+#    #+#             */
+/*   Updated: 2021/06/09 02:33:26 by gutavare         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void 	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}	
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	c;
+
+	c = 'z';
+	while (c >= 'a')
+	{
+		ft_putchar(c);
+		c--;
+	}
+}
